df2cd4f11fe9d799b872b89b06d1e616e592a705
iamdumonde <martinaboumonde@gmail.com>
2023-10-04 18:21:50 +0100

    first commit

---
+++ timers.js
@@ -0,0 +1,31 @@
+'use strict'
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    const spanChrono = document.querySelector('#timers_div #chron');
+    console.log(spanChrono);
+
+    const startButton = document.querySelector('#btn_div #btn_start');
+    console.log(startButton);
+
+    startButton.addEventListener('click', function(){
+        quizzTimer(10); 
+    })
+
+    //Mettre en place la fonction de décompte
+    function quizzTimer(timer){
+        let doubleN = String(timer).padStart(2, '0');
+        spanChrono.innerText = doubleN;
+    
+        if(timer > 0){
+            timer -= 1;
+        }
+    
+        //répétition du comptage
+        setTimeout(function (){
+            quizzTimer(timer);
+        }, 1000 * 60);
+    }
+
+
+})
